1d14a8fe3577361a10f931e8c6437a9dfb0657e0
artemmad <artemmakarevich1997@gmail.com>
2021-07-21 10:05:38 +0300

    Implemented activity_main.xml

---
+++ app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.synergy.filewriterrider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String FILE_NAME = "content.txt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // сохранение файла
+}
