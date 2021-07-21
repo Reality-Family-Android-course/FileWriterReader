3ad2ae52a04651e91cdcd77c4bc2b673dbe260d8
artemmad <artemmakarevich1997@gmail.com>
2021-07-21 10:41:30 +0300

    File external writing finished successfully

--- app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
+++ app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
@@ -24,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private File getExternalFilePath(){
+        return new File(getExternalFilesDir(null), FILE_NAME);
+    }
+
     // сохранение файла
     public void saveText(View view) {
         FileOutputStream fos = null;
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             EditText textBox = (EditText) findViewById(R.id.editor);
             String text = textBox.getText().toString();
 
-            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos =  new FileOutputStream(getExternalFilePath());//openFileOutput(FILE_NAME, MODE_PRIVATE);
             fos.write(text.getBytes());
             Toast.makeText(this, "Файл успешно сохранен", Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
@@ -59,9 +63,10 @@ public class MainActivity extends AppCompatActivity {
     public void openText(View view){
         FileInputStream fin = null;
         TextView textView = (TextView) findViewById(R.id.text);
+        File file = getExternalFilePath();
 
         try{
-            fin = openFileInput(FILE_NAME);
+            fin = new  FileInputStream(file);//openFileInput(FILE_NAME);
             byte[] bytes = new byte[fin.available()];
             fin.read(bytes);
             String text = new String(bytes);
